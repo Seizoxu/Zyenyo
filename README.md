e6100bf9c28561795a14d1184631dd02c914eff6
Xavier Schiller <22575255+XavierSchiller@users.noreply.github.com>
2020-11-18 03:18:42 +0530

    Comments and Small Fixes.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Zyenyo
 This is a simple Discord bot I am creating for fun.
+
+
+# Running the BOT
+
+The bot is programmed to run as a module, hence you'll need to use the `-m` flag to run it with python.
+
+## On Windows
+
+```ps
+$env:BOT_TOKEN='BAR'; python -m ZyenyoBot
+```
+
+## On Linux
+
+```bash
+BOT_TOKEN="BAR" python -m ZyenyoBot
+```
+
+
+## Notes for Sei-chan
+
+I detail a bunch of smaller things
+
+FIXME(BETTER WAY): There's a better way to do this, in python, as adviced by many programmers. Consider researching on how to do this.
+    (I'll also be providing some helpful stuff to refer to so don't worry too much.)
+
+FIXME(DO NOT DO THIS): Do not do [Whatever it is I mentioned]. Typically bad usage of something or the other. 
+
+FIXME(SOME COMMENTS PLEASE): Add a comment or two, it'll be helpful in the long run. 
+
+FIXME(BREAK ME UP): Don't actually break up with someone, you need to just break it up into smaller functions.
--- ZyenyoBot/index.py
+++ ZyenyoBot/index.py
@@ -2,106 +2,137 @@ import discord, os, json, ast
 from discord.ext import commands
 from shutil import copyfile
 from re import sub
-import botconfig
+from . import botconfig
 
-client = commands.Bot(command_prefix = botconfig.PREFIX)
+# I'm importing something here, the fix-me at line 72 should tell you why,
+from .cogs.ping import ping
+
+
+client = commands.Bot(command_prefix=botconfig.PREFIX)
+
+# This is usually how you'd want to register cogs, not using load_extensions (load_extensions is messy.)
+# Also Documentation Time: https://discordpy.readthedocs.io/en/latest/ext/commands/cogs.html#cog-registration
+client.add_cog(ping(client))
 
 ###################
 ###[DEFINITIONS]###
 ###################
-def replace_line(file_name, line_num, text):
-    lines = open(file_name, 'r').readlines()
+def replace_line(file_name, line_num, text): 
+    lines = open(file_name, "r").readlines()
     lines[line_num] = text
-    out = open(file_name, 'w')
+    out = open(file_name, "w")
     out.writelines(lines)
     out.close()
 
-def zbauto(server_name, channel_name):
-    temp = open('../ZBotData/GroupCC/ZBA.json')
+
+def zbauto(server_name, channel_name): # FIXME: There's something fishy about this name :P, three guesses as to what.
+    temp = open("ZBotData/GroupCC/ZBA.json")
     zba = json.loads(temp.readline())
     temp.close()
-    entry = f'{server_name} - {channel_name}'
+    entry = f"{server_name} - {channel_name}"
     global file_number
     if entry in zba:
         zba[entry] = zba[entry] + 1
         file_number = zba[entry]
-        replace_line("../ZBotData/GroupCC/ZBA.json", 0, json.dumps(zba))
+        replace_line("ZBotData/GroupCC/ZBA.json", 0, json.dumps(zba))
     else:
         zba[entry] = 1
-        replace_line("../ZBotData/GroupCC/ZBA.json", 0, json.dumps(zba))
+        replace_line("ZBotData/GroupCC/ZBA.json", 0, json.dumps(zba))
         file_number = 1
 
-temp = open('../ZBotData/char_count_DB.json')
+
+# FIXME: Ideally, you'd want this on the top of the file, right after your imports.
+# Why? There's something known as global variables, and usually they're defined at the top of the file.
+# They're also usually FULL CAPITALS with underscores to make them obvious.
+
+# FIXME(A BETTER WAY): There's a better way to do this: Python Docs on the following:
+# https://docs.python.org/3/reference/compound_stmts.html#the-with-statement
+temp = open("ZBotData/char_count_DB.json")
 chc = json.loads(temp.readline())
 temp.close()
 
+
 @client.command()
-async def load(ctx, extension):
+async def load(ctx, extension): #FIXME(Naming): Load What exactly? 
     if ctx.author.id == 642193466876493829:
-        client.load_extension(f'cogs.{extension}')
+        client.load_extension(f"cogs.{extension}")
         await ctx.send("Successfully loaded the module.")
     else:
         await ctx.send("Please don't try to break me.")
+
+
 @client.command()
-async def unload(ctx, extension):
+async def unload(ctx, extension): #FIXME(Naming): unload What exactly? 
     if ctx.author.id == 642193466876493829:
-        client.unload_extension(f'cogs.{extension}')
+        client.unload_extension(f"cogs.{extension}")
         await ctx.send("Successfully unloaded the module.")
     else:
         await ctx.send("Don't try to break me.")
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+
+# FIXME(DO NOT DO THIS): And for good reason, while this is what you'd call a quick hack (and it works),
+# It's a very bad thing to do. This one is a freebie, from me :)
+# for filename in os.listdir('./cogs'):
+#     if filename.endswith('.py'):
+#         client.load_extension(f'cogs.{filename[:-3]}')
+
 
 ####################
 #####[COMMANDS]#####
 ####################
 
+#FIXME(Style, Flow): So, there's one line that you're doing regardless of condition.
+# So why put them in both blocks? Seperate them.
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
     char_count = len(message.content)
     un = message.author.id
-    if f'{un}_tmc' in chc:
-        chc[f'{un}'] = chc[f'{un}'] + char_count
-        chc[f'{un}_tmc'] = chc[f'{un}_tmc'] + 1
-        replace_line("../ZBotData/char_count_DB.json", 0, json.dumps(chc))
+    if f"{un}_tmc" in chc:
+        chc[f"{un}"] = chc[f"{un}"] + char_count
+        chc[f"{un}_tmc"] = chc[f"{un}_tmc"] + 1
+        replace_line("ZBotData/char_count_DB.json", 0, json.dumps(chc))
     else:
-        chc[f'{un}'] = char_count
-        chc[f'{un}_tmc'] = 1
-        replace_line("../ZBotData/char_count_DB.json", 0, json.dumps(chc))
+        chc[f"{un}"] = char_count
+        chc[f"{un}_tmc"] = 1
+        replace_line("ZBotData/char_count_DB.json", 0, json.dumps(chc))
     await client.process_commands(message)
 
-
+# FIXME(READABILITY) While I love the usage of template strings, it's best used when it's a smaller sentence.
+# The Old += and \n at the end of a string is far more readable.
 @client.command()
 async def stats(message):
     un = message.author.id
-    totalChars = chc[f'{un}']
-    totalMsgs = chc[f'{un}_tmc']
+    totalChars = chc[f"{un}"]
+    totalMsgs = chc[f"{un}_tmc"]
     tAvgChar = round(totalChars / totalMsgs, 2)
-    await message.channel.send(f'Average character count: . . . . . . . . . . **`{tAvgChar}`**\nTotal character count: . . . . . . . . . . . . . **`{totalChars}`**\nTotal messages sent: . . . . . . . . . . . . . . **`{totalMsgs}`**')
-
+    await message.channel.send(
+        f"Average character count: . . . . . . . . . . **`{tAvgChar}`**\nTotal character count: . . . . . . . . . . . . . **`{totalChars}`**\nTotal messages sent: . . . . . . . . . . . . . . **`{totalMsgs}`**"
+    )
 
 
 @client.command()
 async def cstats(message):
     un = message.author.id
-    totalChars = bchc[f'{un}']
-    totalMsgs = bchc[f'{un}_tmc']
+    totalChars = bchc[f"{un}"]
+    totalMsgs = bchc[f"{un}_tmc"]
     tAvgChar = round(totalChars / totalMsgs, 2)
-    await message.channel.send(f'Average character count: . . . . . . . . . . **`{tAvgChar}`**\nTotal character count: . . . . . . . . . . . . . **`{totalChars}`**\nTotal messages sent: . . . . . . . . . . . . . . **`{totalMsgs}`**')
+    await message.channel.send(
+        f"Average character count: . . . . . . . . . . **`{tAvgChar}`**\nTotal character count: . . . . . . . . . . . . . **`{totalChars}`**\nTotal messages sent: . . . . . . . . . . . . . . **`{totalMsgs}`**"
+    )
+
 
 ## bchc is Big-CHaracter-Count.
 
+
 @client.command()
-async def carch(ctx, arg1, arg2):
+async def carch(ctx, arg1, arg2): #FIXME(Naming) What *is* carch.
     arh = sub("<#|>", "", arg1)
     channel = client.get_channel(int(arh))
     server = ctx.message.guild.name
-    copyfile('../ZBotData/cccopy.json', '../ZBotData/c.json')
-    temper = open('../ZBotData/c.json')
+    copyfile("ZBotData/cccopy.json", "ZBotData/c.json")
+    temper = open("ZBotData/c.json")
     global bchc
     bchc = json.loads(temper.readline())
     temper.close()
@@ -111,17 +142,16 @@ async def carch(ctx, arg1, arg2):
         else:
             char_count = len(message.content)
             un = message.author.id
-            if f'{un}_tmc' in bchc:
-                bchc[f'{un}'] = bchc[f'{un}'] + char_count
-                bchc[f'{un}_tmc'] = bchc[f'{un}_tmc'] + 1
-                replace_line("../ZBotData/c.json", 0, json.dumps(bchc))
+            if f"{un}_tmc" in bchc:
+                bchc[f"{un}"] = bchc[f"{un}"] + char_count
+                bchc[f"{un}_tmc"] = bchc[f"{un}_tmc"] + 1
+                replace_line("ZBotData/c.json", 0, json.dumps(bchc))
             else:
-                bchc[f'{un}'] = char_count
-                bchc[f'{un}_tmc'] = 1
-                replace_line("../ZBotData/c.json", 0, json.dumps(bchc))
+                bchc[f"{un}"] = char_count
+                bchc[f"{un}_tmc"] = 1
+                replace_line("ZBotData/c.json", 0, json.dumps(bchc))
     zbauto(server, channel)
-    os.rename('../ZBotData/c.json', f'../ZBotData/GroupCC/{server} - {channel} ({file_number}).json')
+    os.rename(
+        "ZBotData/c.json", f"ZBotData/GroupCC/{server} - {channel} ({file_number}).json"
+    )
     await ctx.send("Done!")
-
-
-client.run(botconfig.token)
