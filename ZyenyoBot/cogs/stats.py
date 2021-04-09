5e5d7bbd38ca4bacc188e0198b4d1a303bef161f
Seizoxu <66660680+Seizoxu@users.noreply.github.com>
2021-04-08 23:05:44 -0500

    Add Activity LB.

--- ZyenyoBot/cogs/stats.py
+++ ZyenyoBot/cogs/stats.py
@@ -87,15 +87,15 @@ class Statistics(commands.Cog):
             if message.author.bot:
                 pass
             else:
-                char2_count = len(message.content)
+                character_count = len(message.content)
                 username = str(message.author.id)
                 if username in SCRAPER_CHAR_INDEX:
-                    SCRAPER_CHAR_INDEX[username] = SCRAPER_CHAR_INDEX[username] + char2_count
+                    SCRAPER_CHAR_INDEX[username] = SCRAPER_CHAR_INDEX[username] + character_count
                     SCRAPER_CHAR_INDEX[username + "_tmc"] = SCRAPER_CHAR_INDEX[username + "_tmc"] + 1
                 else:
-                    SCRAPER_CHAR_INDEX[username] = char2_count
+                    SCRAPER_CHAR_INDEX[username] = character_count
                     SCRAPER_CHAR_INDEX[username + "_tmc"] = 1
-                SCRAPER_CHAR_INDEX["tcc"] = SCRAPER_CHAR_INDEX["tcc"] + char2_count
+                SCRAPER_CHAR_INDEX["tcc"] = SCRAPER_CHAR_INDEX["tcc"] + character_count
                 SCRAPER_CHAR_INDEX["tmc"] = SCRAPER_CHAR_INDEX["tmc"] + 1
                 REPLACE_LINE("ZBotData/c.json", 0, json.dumps(SCRAPER_CHAR_INDEX))
         
@@ -111,7 +111,7 @@ class Statistics(commands.Cog):
         )
 
 
-    #mstats are whatever data the bot has collected passively (lines 42-70).
+    # mstats are whatever data the bot has collected passively (lines 42-70).
     @commands.command(aliases=["mstats"])
     async def messagestatistics(self, ctx):
         username = str(ctx.author.id)
@@ -171,18 +171,53 @@ class Statistics(commands.Cog):
         charKeys = list(charLB.keys())
         charValues = list(charLB.values())
         lbMessage = f"Showing {page * 20 - 19} to {page * 20} of the total characters leaderboard.\n"
-        userTags = []
+        usernames = []
         
         # Make sure all deleted users are handled.
         for i in range((page * 20 - 20), (page * 20)):
             try:
-                userTags.append(await self.client.fetch_user(int(charKeys[i])))
+                usernames.append(await self.client.fetch_user(int(charKeys[i])))
             except Exception as e:
-                userTags.append("deletedUser#0000")
+                usernames.append("deletedUser#0000")
         
         # Format the message, and send it.
         for i in range((page * 20 - 20), (page * 20)):
             if i < (page * 20) and i >= (page * 20 - 20):
-                userTag = userTags[i - ((page - 1) * 20)]
-                lbMessage = lbMessage + f"{i + 1}. {userTag}: **`{charValues[i]}`**\n"
+                username = usernames[i - ((page - 1) * 20)]
+                lbMessage = lbMessage + f"{i + 1}. {username}: **`{charValues[i]}`**\n"
+        await ctx.send(lbMessage)
+    
+    
+    @commands.command(aliases=["alb"])
+    async def activityleaderboard(self, ctx, page):
+        activityLB = {}
+        activityKeys = list(ACTIVITY_INDEX.keys())
+        page = int(page)
+        if page > 10: page = 10
+        
+        # Edit and sort the dictionary.
+        for i in range(len(ACTIVITY_INDEX)):
+            if (i % 2) == 0:
+                activityLB.update({activityKeys[i]: ACTIVITY_INDEX[activityKeys[i]]})
+        activityLB = dict(sorted(activityLB.items(), key = lambda kv: kv[1], reverse=True))
+        del activityLB["tas"]
+        
+        # Getting ready to format the message.
+        activityKeys = list(activityLB.keys())
+        activityValues = list(activityLB.values())
+        lbMessage = f"Showing {page * 20 - 19} to {page * 20} of the total activity leaderboard.\n"
+        usernames = []
+        
+        # Make sure all deleted users are handled.
+        for i in range((page * 20 - 20), (page * 20)):
+            try:
+                usernames.append(await self.client.fetch_user(int(activityKeys[i])))
+            except Exception as e:
+                usernames.append("deletedUser#0000")
+        
+        # Format the message, and send it.
+        for i in range((page * 20 - 20), (page * 20)):
+            if i < (page * 20) and i >= (page * 20 - 20):
+                username = usernames[i - ((page - 1) * 20)]
+                lbMessage = lbMessage + f"{i + 1}. {username}: **`{activityValues[i]}`**\n"
         await ctx.send(lbMessage)
