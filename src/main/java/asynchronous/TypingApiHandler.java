ef553ff585c4780b1ddfccb0e3cb9ec8adf21974
Ashwin Rajesh <46510831+VanillaViking@users.noreply.github.com>
2022-09-02 00:51:28 +1000

    Adding tests now use the database directly (#16)
    * Added connection to database
    
    * Adding tests now use the database directly

--- src/main/java/asynchronous/TypingApiHandler.java
+++ src/main/java/asynchronous/TypingApiHandler.java
@@ -18,16 +18,4 @@ public class TypingApiHandler
 		return response.body();
 	}
 	
-	public static String sendTest(long discordID, double wordsPerMinute, double accuracy, double typingPoints) throws IOException, InterruptedException
-	{
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-			      .uri(URI.create(String.format("https://vanillaviking-type-api.herokuapp.com/test/add/%d", discordID)))
-			      .header("Content-Type", "application/json")
-			      .POST(HttpRequest.BodyPublishers.ofString(String.format(
-			    		  "{\"wpm\": %f, \"accuracy\": %f, \"tp\": %f}", wordsPerMinute, accuracy, typingPoints)))
-			      .build();
-		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		return response.body();
-	}
 }
