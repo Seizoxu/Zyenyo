ab0e35f5489c2647dbe410e9725258547ce7789d
Seizoxu <66660680+Seizoxu@users.noreply.github.com>
2022-10-04 21:38:46 -0500

    Slight Formatting Changes

--- src/main/java/asynchronous/typing/TeamTypingTest.java
+++ src/main/java/asynchronous/typing/TeamTypingTest.java
@@ -1,40 +1,36 @@
 package asynchronous.typing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-import dataStructures.TypingSubmission;
-
-import zyenyo.Database;
-
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import java.util.Arrays;
 import commands.Typing;
+import dataStructures.TypingSubmission;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class TeamTypingTest extends TypingTest
 {
-  private String[] teamA;
-  private String[] teamB;
+	private String[] teamA;
+	private String[] teamB;
 
-  public TeamTypingTest(MessageReceivedEvent event, String[] args) {
-    super(event, args);
-    String[] players = Arrays.copyOfRange(args, 1, args.length);
+	public TeamTypingTest(MessageReceivedEvent event, String[] args)
+	{
+		super(event, args);
+		String[] players = Arrays.copyOfRange(args, 1, args.length);
 
-    teamA = Arrays.copyOfRange(players,0, (int)players.length/2);
-    teamB = Arrays.copyOfRange(players, (int)players.length/2, players.length);
+		teamA = Arrays.copyOfRange(players,0, (int)players.length/2);
+		teamB = Arrays.copyOfRange(players, (int)players.length/2, players.length);
+	}
 
-  }
-
-  @Override
-  public void run() throws NumberFormatException
-  {
-    constructAndSendTest("none");
-    event.getJDA().addEventListener(this);
-  }
-        @Override
+	@Override
+	public void run() throws NumberFormatException
+	{
+		constructAndSendTest("none");
+		event.getJDA().addEventListener(this);
+	}
+	@Override
 	public void quitTest()
 	{
 		scheduledStop.cancel(true);
@@ -61,21 +57,21 @@ public class TeamTypingTest extends TypingTest
 					.sorted((a,b)->Double.compare(submissions.getSubmission(b).typingPoints(), submissions.getSubmission(a).typingPoints()))
 					.collect(Collectors.toList());
 
-                        String team;
-                        double teamATotal = 0;
-                        double teamBTotal = 0;
+			String team;
+			double teamATotal = 0;
+			double teamBTotal = 0;
 
 			for (int i = 0; i < submissions.getNumSubmissions(); i++)
 			{
 
 
 				TypingSubmission s = submissions.getSubmission(lbOrder.get(i));
-                                System.out.println(s.userID());
-                                Arrays.asList(teamA).forEach((String a) -> System.out.println(a));
-                                Arrays.asList(teamB).forEach((String a) -> System.out.println(a));
-                                if (Arrays.asList(teamA).contains(String.format("<@%s>", s.userID()))) {team = "team A"; teamATotal += s.typingPoints();}
-                                else if (Arrays.asList(teamB).contains(String.format("<@%s>", s.userID()))) {team = "team B"; teamBTotal += s.typingPoints();}
-                                else {continue;}
+				System.out.println(s.userID());
+				Arrays.asList(teamA).forEach((String a) -> System.out.println(a));
+				Arrays.asList(teamB).forEach((String a) -> System.out.println(a));
+				if (Arrays.asList(teamA).contains(String.format("<@%s>", s.userID()))) {team = "team A"; teamATotal += s.typingPoints();}
+				else if (Arrays.asList(teamB).contains(String.format("<@%s>", s.userID()))) {team = "team B"; teamBTotal += s.typingPoints();}
+				else {continue;}
 				//double rawTp = Database.addTest(s.userID(), s.wordsPerMinute(), s.accuracy(), s.typingPoints());
 
 				embed.addField(
@@ -89,18 +85,16 @@ public class TeamTypingTest extends TypingTest
 
 			}
 
-                        embed.addField("Team Results", String.format("*Team A Total:* **`%.2f`**%n"
-                                                                                                + "*Team B Total:* **`%.2f`%n**", 
-                                                                                                teamATotal, teamBTotal), false);
-                        double difference = teamATotal - teamBTotal;
-                        String winner = difference > 0 ? "A" : "B";
+			embed.addField("Team Results", String.format("*Team A Total:* **`%.2f`**%n"
+					+ "*Team B Total:* **`%.2f`%n**", 
+					teamATotal, teamBTotal), false);
+			double difference = teamATotal - teamBTotal;
+			String winner = difference > 0 ? "A" : "B";
 
-                        embed.addField(String.format("**Team %s Wins By: `%.2f` Points!**", winner, Math.abs(difference)), "", false);
+			embed.addField(String.format("**Team %s Wins By: `%.2f` Points!**", winner, Math.abs(difference)), "", false);
 
 			message.replyEmbeds(embed.build()).queue();
 			Typing.guildTestList.remove(event.getGuild().getIdLong());
 		}
 	};
-
-
 }
