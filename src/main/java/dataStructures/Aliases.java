33f06296b98c8bd3df641ce2dfceef23ca03a053
Vanilla Viking <ashwinr2k2@gmail.com>
2023-04-25 11:40:18 +1000

    number row practice typing test

--- src/main/java/dataStructures/Aliases.java
+++ src/main/java/dataStructures/Aliases.java
@@ -41,6 +41,7 @@ public final class Aliases
 			BotConfig.PREFIX + "ttest",
 			BotConfig.PREFIX + "tt");
 	public final static Set<String> TEAMVS = Collections.singleton(BotConfig.PREFIX + "teamvs");
+	public final static Set<String> NUMROWTEST = Collections.singleton(BotConfig.PREFIX + "numrow");
 	public final static Set<String> TYPEQUIT = Set.of(
 			BotConfig.PREFIX + "typestop",
 			BotConfig.PREFIX + "typequit",
