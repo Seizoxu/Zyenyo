295795d1b925bd98d24338f3d8a7885a2f0e223e
Ashwin Rajesh <46510831+VanillaViking@users.noreply.github.com>
2023-11-27 09:57:43 +1100

    fix: compare command name & some logging fixes (#92)

--- pull.sh
+++ pull.sh
@@ -11,13 +11,16 @@ deploy() {
     logfile=logs/$(get_logstamp).log
     mkdir -p logs/
 
+    echo "Logs written to: logs/$logfile"
+
     git pull origin stable >> "$logfile" 2>&1
 
-    rm target/*.jar
+    rm target/*.jar 2>&1
     mvn package >> "$logfile" 2>&1
     pkill -f "java -cp $jarfile zyenyo.Zyenyo $BOT_TOKEN $MONGO_URI $ZYENYO_ENVIRONMENT"
 
     jarfile=$(find target/ -name zyenyo-*.*.*.jar)
+    echo "JAR file: $jarfile" >> "$logfile" 2>&1
 
     echo "[TERM] Starting Zyenyo..." >> "$logfile" 2>&1
     java -cp $jarfile zyenyo.Zyenyo $BOT_TOKEN $MONGO_URI $ZYENYO_ENVIRONMENT >> "$logfile" 2>&1
