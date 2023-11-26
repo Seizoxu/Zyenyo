472c85d966493ce79ceeaaaf24cdd6720ba31dc7
Ashwin Rajesh <46510831+VanillaViking@users.noreply.github.com>
2023-11-27 10:37:08 +1100

    fix: get some sleep (#93)

--- pull.sh
+++ pull.sh
@@ -24,6 +24,7 @@ deploy() {
 
     echo "[TERM] Starting Zyenyo..." >> "$logfile" 2>&1
     java -cp $jarfile zyenyo.Zyenyo $BOT_TOKEN $MONGO_URI $ZYENYO_ENVIRONMENT >> "$logfile" 2>&1
+    sleep 5
 }
 
 # Run once first.
