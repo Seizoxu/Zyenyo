cf65c9de7afcba1c9a508793285c1d9ccbfb7bdb
Ashwin Rajesh <46510831+VanillaViking@users.noreply.github.com>
2023-12-25 14:43:48 +1100

    Add FrontEnd, Backend, Dockerized (#97)
    * testing001
    
    * Dockerized 2
    
    * rm testing file
    
    * feat: add readme
    
    ---------
    
    Co-authored-by: Seizoxu <66660680+Seizoxu@users.noreply.github.com>

---
+++ zyenyo-frontend/src/components/TestComponent.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import axios from "axios";
+import {useEffect, useState} from "react"
+
+type Response = {
+ping: String,
+}
+
+const TestComponent = () => {
+    const [test, setTest] = useState<String>("");
+
+    useEffect(() => {
+        async function query() {
+            const res = await axios.get("http://localhost:8000")
+
+            const response = res.data as Response
+            setTest(response.ping)
+        }
+
+        query()
+    }, [])
+
+    return (
+        <div>
+            Test Component
+            <div>
+                Server Response: {test}
+            </div>
+        </div>
+    )
+}
+
+export default TestComponent
