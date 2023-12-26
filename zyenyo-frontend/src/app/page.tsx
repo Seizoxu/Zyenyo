ba7e38ccb424c66bb71b29d158e8dbc473105017
Vanilla Viking <ashwinr2k2@gmail.com>
2023-12-26 16:47:59 +1100

    feat: Home Page

--- zyenyo-frontend/src/app/page.tsx
+++ zyenyo-frontend/src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import axios from 'axios'
 import TestComponent from '@/components/TestComponent'
+import Dashboard from '@/components/HomePage/Dashboard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-[#1E1F22] text-white">
-      <div className='flex flex-row gap-0 text-9xl mb-10'>
-        <div className='text-white'>Zyenyo</div><div className='text-[#F51A1F]'>Bot</div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center p-24 bg-zinc-900 text-white">
+
+      <Dashboard/>
+
       <TestComponent />
     </main>
   )
